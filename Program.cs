0c776d22798e911bd20bd11ef1a21cbc954134bf
Mattia-Zanini <mattzanini2@gmail.com>
2021-10-08 22:50:44 +0200

    Inizio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System;
+using System.IO;
+
+namespace Fantacalcio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] calciatori = File.ReadAllLines("Calciatori.txt");
+            File.WriteAllLines("Calciatori.txt", calciatori);
+            Console.WriteLine("Finito");
+        }
+    }
+}
