b230a68735a65bd14198cf9a24764e6b0dfb673b
Mattia-Zanini <mattzanini2@gmail.com>
2021-10-14 21:02:27 +0100

    Asta, nuovo comando
    il pezzo di codice che si occupa dell'asta è quasi completato, è stato aggiunto un nuovo comando e inoltre il programma controlla se i giocatori formano squadre di 0 calciatori e li punisce :)

--- Program.cs
+++ Program.cs
@@ -13,13 +13,13 @@ namespace Fantacalcio
             string mainFolderPath = SetupDir(dir);
             string[] nomiGiocatori = new string[0];
             Setup(mainFolderPath, ref nomiGiocatori);
-            string comand;
+            string command;
             do
             {
-                Console.Write("<comand>");
-                comand = Console.ReadLine();
-                Comandi(comand, mainFolderPath);
-            } while (comand != "exit");
+                Console.Write("Fantacalcio>");
+                command = Console.ReadLine();
+                Comandi(command, mainFolderPath);
+            } while (command != "exit");
             Console.WriteLine("Il programma si sta chiudendo");
         }
         //in base a ciò che l'utente inserisce come comando lo switch esegue diverse attività e riconosce se un utente non inserisce un comando
@@ -32,14 +32,14 @@ namespace Fantacalcio
                     MostraComandi(mainFolderPath);
                     break;
                 default:
-                    string[] comands = comand.Split(" ");
-                    if (comands[0] == "rgd")
+                    string[] commands = comand.Split(" ");
+                    if (commands[0] == "rgd")
                     {
-                        ControlloComandi(comands);
+                        ControlloComandi(commands);
                     }
                     else
                     {
-                        Console.WriteLine("Comando inesistente");
+                        Console.WriteLine("Comando non trovato o non accessibile in questa situazione");
                     }
                     break;
             }
@@ -50,7 +50,7 @@ namespace Fantacalcio
             for (int i = 0; i < help.Length; i++)
             {
                 string[] str = help[i].Split(',');
-                Console.WriteLine($"{str[0]}\t\t{str[1]}");
+                Console.WriteLine($"{str[0]}  --->  {str[1]}");
             }
         }
         //a seconda del comando scritto dall'utente, che inizia per "rgd", esegue diverse funzioni
@@ -88,6 +88,7 @@ namespace Fantacalcio
                     string[] str2 = str[1].Split(".txt");
                     nomiGiocatori[i] = str2[0];
                 }
+                ControlloSquadreVuote(mainFolderPath, nomiGiocatori);
             }
         }
         //Questa funzione si occupa di ritagliare il percorso del file, al fine di ottenerne un altro
@@ -117,7 +118,7 @@ namespace Fantacalcio
                 }
                 else if (nPlayer <= 1 || nPlayer > 24)
                 {
-                    System.Console.WriteLine("Inserisci un numero maggiore di 1 e minore di 25");
+                    Console.WriteLine("Inserisci un numero maggiore di 1 e minore di 25");
                 }
             } while (!correctSyntax || nPlayer <= 1 || nPlayer > 24);
         }
@@ -153,43 +154,62 @@ namespace Fantacalcio
         {
             return !Directory.EnumerateFileSystemEntries(path).Any();
         }
+        //crea le squadre di ogni giocatore
         private static void SetupSquadreGiocatori(string[] nomiGiocatori, string path)
         {
             bool finito = false;
+            int[] soldiGiocatori = new int[nomiGiocatori.Length];
+            SetupFantacrediti(soldiGiocatori);
             do
             {
                 Console.WriteLine("Scrivi il giocatore che deve essere messo all'asta");
-                string calciatore = Console.ReadLine();
+                string calciatore = Console.ReadLine().ToLower();
                 if (ControlloEsistenzaCalciatore(calciatore, path))
                 {
-                    Asta(calciatore);
+                    Asta(calciatore, nomiGiocatori, path, soldiGiocatori);
+                }
+                else if (calciatore == "exitasta")
+                {
+                    finito = true;
                 }
                 else
                 {
-                    Console.WriteLine("Calciatore non trovato");
+                    Console.WriteLine("Calciatore non trovato\n");
                 }
             } while (!finito);
+            ControlloSquadreVuote(path, nomiGiocatori);
         }
+        //consegna a tutti i giocatori un tot di crediti con cui possono acquistare i calciatori
+        private static void SetupFantacrediti(int[] soldi)
+        {
+            for (int i = 0; i < soldi.Length; i++)
+            {
+                soldi[i] = 500;
+            }
+        }
+        //controlla se il calciatore che si vuole acquistare esiste
         private static bool ControlloEsistenzaCalciatore(string calciatoreAsta, string path)
         {
             string[] calciatori = File.ReadAllLines(path + "\\Calciatori.txt");
             for (int i = 0; i < calciatori.Length; i++)
             {
                 string[] calciatore = calciatori[i].Split(',');
-                if (calciatoreAsta.ToLower() == calciatore[0]) return true;
+                if (calciatoreAsta == calciatore[0]) return true;
             }
             return false;
         }
-        private static void Asta(string calciatore)
+        //gestisce l'acquisto dei calciatori da parte dei partecipanti
+        private static void Asta(string calciatore, string[] nomiGiocatori, string path, int[] soldiGiocatori)
         {
-            bool offerta = false;
+            bool astaFinita = false;
+            int offertaAsta = 0;
             do
             {
                 Console.WriteLine("Inserisci un offerta");
-                int offertaAsta = 0;
+                string risposta = Console.ReadLine().ToLower();
                 try
                 {
-                    int tmp = int.Parse(Console.ReadLine());
+                    int tmp = Convert.ToInt32(risposta);
                     if (tmp > offertaAsta)
                         offertaAsta = tmp;
                     else
@@ -197,18 +217,76 @@ namespace Fantacalcio
                 }
                 catch
                 {
-                    Console.WriteLine("Prezzo non valido");
+                    if (risposta == "exitasta")
+                    {
+                        AssegnazioneCalciatore(calciatore, nomiGiocatori, path, ref astaFinita, offertaAsta, soldiGiocatori);
+                    }
+                    else
+                        Console.WriteLine("Prezzo non valido");
                 }
-                Console.WriteLine("Interrompere l'asta?");
-                string risposta = Console.ReadLine();
-                ExitAsta(ref risposta, ref offerta);
-            } while (!offerta);
+            } while (!astaFinita);
+            Console.WriteLine($"L'asta per il calciatore: {calciatore} e' conclusa");
         }
-        private static void ExitAsta(ref string risposta, ref bool offerta)
+        //permette ai giocatori di popolare la propria squadra, controlla anche se i giocatori quando vogliono acquistare un calciatore hanno abbastanza crediti
+        private static void AssegnazioneCalciatore(string calciatore, string[] nomiGiocatori, string path, ref bool astaFinita, int offertaAsta, int[] soldiGiocatori)
         {
+            bool nomeCorretto = false;
             do
             {
-            } while (risposta == "si" || risposta == "no");
+                Console.WriteLine("Inserisci il nome dell'utente che ha effettuato l'ultima offerta");
+                string nomePlayer = Console.ReadLine();
+                for (int i = 0; i < nomiGiocatori.Length; i++)
+                {
+                    if (nomePlayer == nomiGiocatori[i])
+                    {
+                        if(soldiGiocatori[i] >= offertaAsta)
+                        {
+                            string[] squadraPlayer = File.ReadAllLines(path + $"\\{nomiGiocatori[i]}.txt");
+                            Array.Resize(ref squadraPlayer, squadraPlayer.Length + 1);
+                            squadraPlayer[squadraPlayer.Length - 1] = calciatore;
+                            File.WriteAllLines(path + $"\\{nomiGiocatori[i]}.txt", squadraPlayer);
+                            AcquistoCalciatore(path, calciatore);
+                            soldiGiocatori[i] -= offertaAsta;
+                            nomeCorretto = true;
+                            astaFinita = true;
+                            break;
+                        }
+                        else
+                            Console.WriteLine($"{nomePlayer} non ha abbastanza crediti, rieffettuare l'asta per questo calciatore"); nomeCorretto = true; astaFinita = true;
+                    }
+                }
+            } while (!nomeCorretto);
+        }
+        //rende un calciatore non più acquistabile, settando la sua variabile a false, nel file
+        private static void AcquistoCalciatore(string path, string calciatore)
+        {
+            string[] calciatori = File.ReadAllLines(path + "\\Calciatori.txt");
+            for (int i = 0; i < calciatori.Length; i++)
+            {
+                string[] tmp = calciatori[i].Split(',');
+                if (calciatore == tmp[0])
+                {
+                    tmp[3] = "true";
+                    calciatori[i] = $"{tmp[0]},{tmp[1]},{tmp[2]},{tmp[3]}";
+                    File.WriteAllLines(path + "\\Calciatori.txt", calciatori);
+                    break;
+                }
+            }
+        }
+        private static void ControlloSquadreVuote(string path, string[] nomiPlayers)//https://stackoverflow.com/questions/61208202/how-to-check-if-a-text-file-is-empty-c-sharp
+        {
+            for(int i = 0; i < nomiPlayers.Length; i++)
+            {
+                string[] tmp = File.ReadAllLines(path + $"\\Squadre\\{nomiPlayers[i]}.txt");
+                if (tmp.Length == 0)
+                {
+                    Console.WriteLine($"{nomiPlayers[i]} non penso che tu possa vincere con una squadra di 0 calciatori");
+                    File.Delete(path + $"\\Squadre\\{nomiPlayers[i]}.txt");
+                }
+            }
+            Console.WriteLine("\nPer pigrizia dello sviluppatore dovrete ricominciare dall'inizio\n");
+            string[] nomiGiocatori = new string[0];
+            Setup(path, ref nomiGiocatori);
         }
     }
 }
